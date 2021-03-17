6925c6a8a566181dc6280d3b543e16672afa763b
Madhanrajan <madhanrajan.varadharajan@gmail.com>
2021-03-17 23:34:30 +0800

    fixed eps_p_list

--- qfactor/transfer_matrix.py
+++ qfactor/transfer_matrix.py
@@ -1,68 +1,74 @@
 import numpy as np
 from .models import Element
 
-def kz_N(eps_N,wl,theta): 
-    return (2*(np.pi/wl)**2*eps_N-kx_sub(wl,theta)**2)**0.5 
 
-#kx sub is 2pi/lamba0 * sin ( incident angle)
+def kz_N(eps_N, wl, theta):
+    return (2*(np.pi/wl)**2*eps_N-kx_sub(wl, theta)**2)**0.5
+
+# kx sub is 2pi/lamba0 * sin ( incident angle)
+
+
 def kx_sub(wl, theta):
     return 2*np.pi/wl * np.sin(theta)
 
-def q_N(eps_N,wl,theta):
-    return kz_N(eps_N,wl,theta)/eps_N  
 
-def m11_N(d_N, eps_N,wl,theta):
-    return np.cos(kz_N(eps_N,wl,theta)*d_N)
-
-def m12_N(d_N,eps_N,wl,theta):
-    return -1j/q_N(eps_N,wl,theta)*np.sin(kz_N(eps_N,wl,theta)*d_N)
-
-def m21_N(d_N, eps_N,wl,theta):
-    return -1.0j*q_N(eps_N,wl,theta)*np.sin(kz_N(eps_N,wl,theta)*d_N)
-
-def m22_N(d_N, eps_N,wl,theta):
-    return np.cos(kz_N(eps_N,wl,theta)*d_N)
+def q_N(eps_N, wl, theta):
+    return kz_N(eps_N, wl, theta)/eps_N
 
 
-def M_N(d_N, eps_N,wl,theta):
-    return np.array([[m11_N(d_N,eps_N,wl,theta),m12_N(d_N,eps_N,wl,theta)],[m21_N(d_N,eps_N,wl,theta),m22_N(d_N,eps_N,wl,theta)]])
+def m11_N(d_N, eps_N, wl, theta):
+    return np.cos(kz_N(eps_N, wl, theta)*d_N)
 
 
-def kz_sub(eps_sub,wl,theta):
-    return ((2.*np.pi/wl)**2*eps_sub-kx_sub(wl,theta)**2)**(1/2)
+def m12_N(d_N, eps_N, wl, theta):
+    return -1j/q_N(eps_N, wl, theta)*np.sin(kz_N(eps_N, wl, theta)*d_N)
 
-def q_sub(eps_sub,wl, theta):
-    return kz_sub(eps_sub,wl,theta)/eps_sub
 
-def kz_super(eps_super,wl,theta):
-    return ((2*np.pi/wl)**2*eps_super-kx_sub(wl,theta)**2)**0.5
+def m21_N(d_N, eps_N, wl, theta):
+    return -1.0j*q_N(eps_N, wl, theta)*np.sin(kz_N(eps_N, wl, theta)*d_N)
 
-def q_super(eps_super,wl,theta):
-    return kz_super(eps_super,wl,theta)/eps_super
-    
+
+def m22_N(d_N, eps_N, wl, theta):
+    return np.cos(kz_N(eps_N, wl, theta)*d_N)
+
+
+def M_N(d_N, eps_N, wl, theta):
+    return np.array([[m11_N(d_N, eps_N, wl, theta), m12_N(d_N, eps_N, wl, theta)], [m21_N(d_N, eps_N, wl, theta), m22_N(d_N, eps_N, wl, theta)]])
+
+
+def kz_sub(eps_sub, wl, theta):
+    return ((2.*np.pi/wl)**2*eps_sub-kx_sub(wl, theta)**2)**(1/2)
+
+
+def q_sub(eps_sub, wl, theta):
+    return kz_sub(eps_sub, wl, theta)/eps_sub
+
+
+def kz_super(eps_super, wl, theta):
+    return ((2*np.pi/wl)**2*eps_super-kx_sub(wl, theta)**2)**0.5
+
+
+def q_super(eps_super, wl, theta):
+    return kz_super(eps_super, wl, theta)/eps_super
+
 # sub = first layer   to find the eps = (n^2+k^2)^1/2
-def eps_p(omega,theta, E, eps_sub):
-    Exy_real, Exy_im, Ez_real, Ez_im = E
-    epsilon_effx = complex(Exy_real,Exy_im)
-    epsilon_effz = complex(Ez_real,Ez_im)
+
+
+def eps_p(Exy_r, Exy_im, Ez_r, Ez_im, theta, eps_sub):
+
+    epsilon_effx = complex(Exy_r, Exy_im)
+    epsilon_effz = complex(Ez_r, Ez_im)
     return epsilon_effx + (eps_sub*sind(theta)**2)*(1-epsilon_effx/epsilon_effz)
+
 
 def calculate_M(layers, theta, params, E, interpolate):
 
-    
     # big M = []
 
     # for w in wl:
     #     small_M = np.array([[1,1],[1,1]])
     #     for i in range(len(layers)):
     #         material, thickness = layers[i]
-            
-    
-
-        
-
-       
-
 
     #     epsp = eps_p(2*np.pi/wl*3.0*10**8, theta,E,eps_sub)
     #     m_n = M_N(thickness, epsp,wl,theta)
@@ -71,29 +77,31 @@ def calculate_M(layers, theta, params, E, interpolate):
     print("reflection")
     # for w in wl:
     #     print(get_reflection(E, M,theta,wl, eps_super, eps_sub))
-        
-    
-def get_reflection(M,theta,wl, eps_super, eps_sub):
-    m11 = M[0,0]
-    m12 = M[0,1]
-    m21 = M[1,0]
-    m22 = M[1,1]
 
-    r = ((m11 + m12*q_super(eps_super,wl,theta))*q_sub(eps_sub,wl,theta) - (m21+m22*q_super(eps_super,wl,theta)))/((m11+m12*q_super(eps_super,wl,theta))*q_sub(eps_sub,wl,theta) + (m21*m22*q_super(eps_super,wl,theta)))
+
+def get_reflection(M, theta, wl, eps_super, eps_sub):
+    m11 = M[0, 0]
+    m12 = M[0, 1]
+    m21 = M[1, 0]
+    m22 = M[1, 1]
+
+    r = ((m11 + m12*q_super(eps_super, wl, theta))*q_sub(eps_sub, wl, theta) - (m21+m22*q_super(eps_super, wl, theta))) / \
+        ((m11+m12*q_super(eps_super, wl, theta))*q_sub(eps_sub,
+                                                       wl, theta) + (m21*m22*q_super(eps_super, wl, theta)))
     return r
 
-def get_transmission(M, theta, wl,eps_super,eps_sub):   
-    m11 = M[0,0]
-    m12 = M[0,1]
-    m21 = M[1,0]
-    m22 = M[1,1]
 
-    t=2*q_sub(eps_sub,wl,theta)/((m11 + m12* q_super(eps_super,wl,theta))*q_sub(eps_sub,wl,theta) + (m21 + m22*q_super(eps_super,wl,theta)))
+def get_transmission(M, theta, wl, eps_super, eps_sub):
+    m11 = M[0, 0]
+    m12 = M[0, 1]
+    m21 = M[1, 0]
+    m22 = M[1, 1]
 
+    t = 2*q_sub(eps_sub, wl, theta)/((m11 + m12 * q_super(eps_super, wl, theta))
+                                     * q_sub(eps_sub, wl, theta) + (m21 + m22*q_super(eps_super, wl, theta)))
 
-    return t 
-   
+    return t
+
 
 def sind(theta):
     return np.sin(theta/np.pi*180)
-
--- qfactor/transfer_matrix_2.py
+++ qfactor/transfer_matrix_2.py
@@ -1,67 +1,68 @@
 
 from .transfer_matrix import *
-                         
-def magic(E,n,k,wl,params,theta,layers):
-    wv_val = [0]*int(params["number_of_layers"]) 
+
+
+def magic(E, n, k, wl, params, theta, layers):
+    wv_val = [0]*int(params["number_of_layers"])
     L = [0]*int(params["number_of_layers"])
-
-
 
     epsil_real = []
     epsil_imag = []
-    q=[0]*int(params["number_of_layers"]) 
-    
+    q = [0]*int(params["number_of_layers"])
+
     Exy_real, Exy_im, Ez_real, Ez_im = E
 
     n_ = np.array(n)
     k_ = np.array(k)
 
+    eps_p_list_real = []
+    eps_p_list_im = []
+
+    for i in range(len(Exy_real)):
+        eps_p_list_real.append(
+            eps_p(Exy_real[i], Exy_im[i], Ez_real[i], Ez_im[i], theta, 1).real)
+        eps_p_list_im.append(
+            eps_p(Exy_real[i], Exy_im[i], Ez_real[i], Ez_im[i], theta, 1).imag)
 
     for i in range(len(layers)):
         material, thickness = layers[i]
-        if material== "metamaterial":
-            epsil_real = np.array(Exy_real)# + np.array(Ez_real)
-            epsil_imag = np.array(Exy_im)# + np.array(Ez_im)
-            
-            
+        if material == "metamaterial":
+            epsil_real = np.array(eps_p_list_real)
+            epsil_imag = np.array(eps_p_list_im)
+
         else:
             epsil_real = (n_**2)-(k_**2)
             epsil_imag = (2*n_*k_)
-            
-        L[i]=float(thickness)      
 
-    
-    
+        L[i] = float(thickness)
+
     big_M = []
-    
+
     for i in range(len(wl)):
         w = wl[i]
-        little_M = np.array([[1,0],[0,1]])
+        little_M = np.array([[1, 0], [0, 1]])
 
         for j in range(int(params["number_of_layers"])):
-            material, thickness = layers[j] 
+            material, thickness = layers[j]
 
+            # setting the epsil values for each layer
+            epsil = complex(epsil_real[j], epsil_imag[j])
 
-            #setting the epsil values for each layer  
-            epsil = complex(epsil_real[j],epsil_imag[j])                                       
-        
             # little_m = np.array([[1,0],[0,1]])
             # wv_val[j]=(2*np.pi/wl[i])*epsil-(2*np.pi)/wl[i]*np.sin(30/np.pi*180)                                                 #need to change the initial wv_val for non-normal incidence. We need phi probably
             # q[j] = wv_val[j]/epsil
-            
+
             # M11 = np.cos(wv_val[j]*L[j]) #matrix elements of layers
             # M12 = complex(0, (1/(q[j]*(np.sin(wv_val[j]*L[j])))))
             # M21 = complex(0,(q[j]*np.sin(wv_val[j]*L[j])))
             # M22 = np.cos(wv_val[j]*L[j])
             # little_m = np.matmul([[M11,M12],[M21,M22]],little_m)
-            M = M_N(float(thickness), epsil,w,theta)
-            little_M = np.matmul(M,little_M)
-             ### <- use thise instead of the one on top
-            
+            M = M_N(float(thickness), epsil, w, theta)
+            little_M = np.matmul(M, little_M)
+            # <- use thise instead of the one on top
+
         big_M.append(little_M)
 
-    
-    
     r_list = []
     big_M = np.array(big_M)
     t_list = []
@@ -70,11 +71,13 @@ def magic(E,n,k,wl,params,theta,layers):
         wavelen = wl[w]
         eps_super = 1
         eps_sub = 1
-        r_list.append(get_reflection(big_M[w],theta,wavelen, eps_super, eps_sub))
-        t_list.append(get_transmission(big_M[w], theta, wavelen,eps_super,eps_sub))
-    
-    t_mag = [ np.sqrt(np.conjugate(x)*x) for x in t_list ]
-    r_mag = [ np.sqrt(np.conjugate(x)*x) for x in r_list ]
+        r_list.append(get_reflection(
+            big_M[w], theta, wavelen, eps_super, eps_sub))
+        t_list.append(get_transmission(
+            big_M[w], theta, wavelen, eps_super, eps_sub))
+
+    t_mag = [np.sqrt(np.conjugate(x)*x) for x in t_list]
+    r_mag = [np.sqrt(np.conjugate(x)*x) for x in r_list]
 
     t_real = [x.real for x in t_list]
     t_imag = [x.imag for x in t_list]
@@ -82,7 +85,4 @@ def magic(E,n,k,wl,params,theta,layers):
     r_real = [x.real for x in r_list]
     r_imag = [x.imag for x in r_list]
 
-    
-
     return t_real, t_imag, r_real, r_imag
-
